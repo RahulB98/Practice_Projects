24f44b11b2bdf83dd44f7239d706952f51a7921b
RahulB98 <51396552+RahulB98@users.noreply.github.com>
2020-11-11 22:00:33 +0530

    today's work

--- Tkinter lessons/Projects/Shop_assist_amazon/my_shop_assist.py
+++ Tkinter lessons/Projects/Shop_assist_amazon/my_shop_assist.py
@@ -1,14 +1,16 @@
 import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
 import ssl
-import csv
 import pandas as pd
+import time
 
-def get_price(url, desired_price):
+def get_price(row, url):
     # Ignore SSL certificate errors
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
+
+    lookup_time = time.now().strftime('%Y-%m-%d %Hh%Mm')
 
     link = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(link, 'html.parser')
@@ -33,15 +35,11 @@ def get_price(url, desired_price):
                 print("Cannot find price")
     article_title = " ".join(title)
     article_price = float(price[1].replace(",", ""))
-    if article_price <= desired_price:
+    if article_price <= file.desired_price[row]:
         print("*****BUY NOW: " + article_title + " ************")
     print(article_title, ": ", article_price)
-'''
-link_1 = ['https://www.amazon.in/gp/product/B07XVMDRZY/ref=s9_acss_bw_cg_Headers_2d1_w?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-12&pf_rd_r=16PQHT6J2KFZW359EV9D&pf_rd_t=101&pf_rd_p=00cb27c4-faf9-47e8-8899-c96f8fbb7ef8&pf_rd_i=1389401031', 50000]
-link_2 = ['https://www.amazon.in/dp/B012TRTE6M/ref=twister_dp_update?_encoding=UTF8&psc=1', 1500]
-link_3 = ['https://www.amazon.in/ASUS-FX505DT-15-6-inch-Graphics-FX505DT-BQ151T/dp/B08CY2M684', 51000]
-url_list = [link_1, link_2, link_3]
-'''
-file = pd.read_csv()
-        #for url, desired_price in url_list:
-        #  get_price(url, desired_price)
+
+file = pd.read_csv('items_list.csv', sep=',')
+urls = file.url
+for row, url in enumerate(urls):
+    get_price(row, url)
